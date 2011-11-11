5968e55a6b8a7eafb9f3c3f6012fc8fcc08cfeda
Shiro <alu@senzaii.net>
2011-11-11 15:11:12 +0100

    [Core/Scripts] Add entry to scriptloader

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -24,6 +24,9 @@ void AddSC_example_gossip_codebox();
 void AddSC_example_misc();
 void AddSC_example_commandscript();
 
+//custom
+void AddSC_dalaran_squirrel();
+
 // spells
 void AddSC_deathknight_spell_scripts();
 void AddSC_druid_spell_scripts();
@@ -1238,6 +1241,7 @@ void AddCustomScripts()
 {
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
+	AddSC_dalaran_squirrel();
 
 #endif
 }
