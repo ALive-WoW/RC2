6a2cb9c5d8e2fd153eb239ffa8062692d6ab2ad9
AliveShiro <alu@senzaii.net>
2011-11-16 18:16:04 +0100

    [Core/Paladin] Add heal effect to divine storm (sql)

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -1134,7 +1134,11 @@ void Spell::EffectDummy(SpellEffIndex effIndex)
                     return;
                 case 54171:                                   //Divine Storm
                 {
-                    m_caster->CastCustomSpell(unitTarget, 54172, &damage, 0, 0, true);
+					if (m_UniqueTargetInfo.size())
+					{
+						int32 heal = damage / m_UniqueTargetInfo.size();
+						m_caster->CastCustomSpell(unitTarget, 54172, &heal, NULL, NULL, true);
+					}
                     return;
                 }
                 case 58418:                                 // Portal to Orgrimmar
@@ -1362,15 +1366,15 @@ void Spell::EffectDummy(SpellEffIndex effIndex)
             break;
         case SPELLFAMILY_PALADIN:
             // Divine Storm
-            if (m_spellInfo->SpellFamilyFlags[1] & SPELLFAMILYFLAG1_PALADIN_DIVINESTORM && effIndex == 1)
-            {
-                int32 dmg = CalculatePctN(m_damage, damage);
-                if (!unitTarget)
-                    unitTarget = m_caster;
-                m_caster->CastCustomSpell(unitTarget, 54171, &dmg, 0, 0, true);
-                return;
-            }
-
+/*            if (m_spellInfo->SpellFamilyFlags[1] & SPELLFAMILYFLAG1_PALADIN_DIVINESTORM && effIndex == 1)
+ *           {
+ *               int32 dmg = CalculatePctN(m_damage, damage);
+ *               if (!unitTarget)
+ *                   unitTarget = m_caster;
+ *               m_caster->CastCustomSpell(unitTarget, 54171, &dmg, 0, 0, true);
+ *               return;
+ *           }
+ */
             switch (m_spellInfo->Id)
             {
                 case 31789:                                 // Righteous Defense (step 1)
